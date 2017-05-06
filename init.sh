3361c9ade10e931da2e39c6bc67a9b4eea053b8b
Satoshi Tanaka <tanarky@gmail.com>
2017-05-06 17:35:47 +0900

    fixed

--- init.sh
+++ init.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
-apt install tree unattended-upgrades aptitude git libbz2-dev libsqlite-dev libreadline-dev build-essential curl wget -y
-unattended-upgrade
-git clone https://github.com/riywo/anyenv /usr/local/anyenv
-echo 'export ANYENV_ROOT="/usr/local/anyenv"' > /tmp/anyenv.sh
-echo 'export PATH="/usr/local/anyenv/bin:$PATH"' >> /tmp/anyenv.sh
-echo 'eval "$(anyenv init -)"' >> /tmp/anyenv.sh
-mv /tmp/anyenv.sh /etc/profile.d/
-source /etc/profile.d/anyenv.sh
-
-anyenv install rbenv && anyenv install pyenv && anyenv install plenv && anyenv install phpenv
-source /etc/profile.d/anyenv.sh
-
-pyenv install 2.7.12 && pyenv global 2.7.12
-source /etc/profile.d/anyenv.sh
-
-plenv install 5.22.1 && plenv global 5.22.1
-source /etc/profile.d/anyenv.sh
-
-pip install --upgrade pip && pip install ansible==2.3.0
-
-source /etc/profile.d/anyenv.sh
-ansible-playbook -i hosts.self self.yml
+apt install tree unattended-upgrades aptitude git build-essential curl wget \
+    libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl \
+    llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev -y && \
+    unattended-upgrade && \
+    git clone https://github.com/riywo/anyenv /usr/local/anyenv && \
+    echo 'export ANYENV_ROOT="/usr/local/anyenv"' > /tmp/anyenv.sh && \
+    echo 'export PATH="/usr/local/anyenv/bin:$PATH"' >> /tmp/anyenv.sh && \
+    echo 'eval "$(anyenv init -)"' >> /tmp/anyenv.sh && \
+    mv /tmp/anyenv.sh /etc/profile.d/ && \
+    source /etc/profile.d/anyenv.sh && \
+    anyenv install rbenv && anyenv install pyenv && anyenv install plenv && anyenv install phpenv && \
+    source /etc/profile.d/anyenv.sh && \
+    pyenv install 2.7.12 && pyenv global 2.7.12  && \
+    source /etc/profile.d/anyenv.sh && \
+    plenv install 5.22.1 && plenv global 5.22.1  && \
+    source /etc/profile.d/anyenv.sh  && \
+    pip install --upgrade pip && pip install ansible==2.3.0 && \
+    source /etc/profile.d/anyenv.sh && \
+    ansible-playbook -i hosts.self self.yml && \
 
 # git ssh: http://qiita.com/sonots/items/826b90b085f294f93acf
 echo '#!/bin/bash
@@ -50,6 +47,6 @@ EOF
     rm -f $tempfile
 else
     git $@
-fi' > /tmp/git_with_deploy_key
-chmod +x /tmp/git_with_deploy_key
+fi' > /tmp/git_with_deploy_key && \
+chmod +x /tmp/git_with_deploy_key && \
 mv /tmp/git_with_deploy_key /usr/local/bin
